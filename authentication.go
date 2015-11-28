be195ea3715b40d186e684531db2c28a15b72609
simon <ampersand8@gmail.com>
2015-11-28 14:40:02 +0100

    implement simple helper for restricting and authentication

--- authentication.go
+++ authentication.go
@@ -6,6 +6,8 @@ import (
     "io/ioutil"
     "log"
     "time"
+    "net/http"
+    "encoding/json"
 )
 
 
@@ -49,6 +51,54 @@ func CreateTokenString(user string) (string, error) {
     // set expire time
     t.Claims["exp"] = time.Now().Add(time.Hour * 1).Unix()
     return t.SignedString(signKey)
+}
+
+func IsAllowed(w http.ResponseWriter, r *http.Request) bool {
+    client_token := r.Header.Get("admintoken")
+    if client_token == "" {
+        w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+        w.WriteHeader(http.StatusForbidden)
+        json.NewEncoder(w).Encode(map[string]string{"error": "token not set"})
+    }
+
+    // validate the token
+    token, err := jwt.Parse(client_token, func(token *jwt.Token) (interface{}, error) {
+        return verifyKey, nil
+    })
+
+    switch err.(type) {
+    case nil: //no error
+        if !token.Valid { // may be invalid
+            w.WriteHeader(http.StatusUnauthorized)
+            json.NewEncoder(w).Encode(map[string]string {"error": "token is invalid"})
+            log.Println("Token is invalid")
+            return false
+        }
+        return true
+
+    case *jwt.ValidationError: // something was wrong during the validation
+        vErr := err.(*jwt.ValidationError)
+
+        switch vErr.Errors {
+        case jwt.ValidationErrorExpired:
+            w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+            w.WriteHeader(http.StatusUnauthorized)
+            json.NewEncoder(w).Encode(map[string]string {"error": "token expired"})
+            return false
+
+        default:
+            w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+            w.WriteHeader(http.StatusUnauthorized)
+            json.NewEncoder(w).Encode(map[string]string {"error": "error while parsing token "})
+            return false
+        }
+
+    default: // something else went wrong
+        w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+        w.WriteHeader(http.StatusInternalServerError)
+        json.NewEncoder(w).Encode(map[string]string {"error": "something went wrong"})
+        return false
+    }
 }
 
 // Dummy Authentication function
